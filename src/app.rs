f69818c845cc2c1b5c334f655b94fc0f3d530e66
SkillerRaptor <skillerraptor@protonmail.com>
2026-09-10 10:59:20 +0200

    feat: add bar window

--- src/app.rs
+++ src/app.rs
@@ -6,7 +6,11 @@
 
 use relm4::{SimpleComponent, gtk::prelude::*, prelude::*};
 
-pub struct AppModel {}
+use crate::bar::BarModel;
+
+pub struct AppModel {
+    _bar: Controller<BarModel>,
+}
 
 #[relm4::component(pub)]
 impl SimpleComponent for AppModel {
@@ -25,7 +29,10 @@ impl SimpleComponent for AppModel {
         root: Self::Root,
         _sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
-        let model = Self {};
+        let bar = BarModel::builder().launch(()).detach();
+
+        let model = Self { _bar: bar };
+
         let widgets = view_output!();
         ComponentParts { model, widgets }
     }
--- src/bar/mod.rs
+++ src/bar/mod.rs
@@ -3,3 +3,61 @@
 //
 // SPDX-License-Identifier: MIT
 //
+
+use gtk4_layer_shell::LayerShell;
+use relm4::{SimpleComponent, gtk::prelude::*, prelude::*};
+
+pub struct BarModel {}
+
+#[relm4::component(pub)]
+impl SimpleComponent for BarModel {
+    type Init = ();
+    type Input = ();
+    type Output = ();
+
+    view! {
+        gtk::Window {
+            init_layer_shell: (),
+            set_layer: gtk4_layer_shell::Layer::Top,
+            auto_exclusive_zone_enable: (),
+
+            set_anchor: (gtk4_layer_shell::Edge::Top, true),
+            set_anchor: (gtk4_layer_shell::Edge::Bottom, false),
+            set_anchor: (gtk4_layer_shell::Edge::Left, true),
+            set_anchor: (gtk4_layer_shell::Edge::Right, true),
+
+            set_margin: (gtk4_layer_shell::Edge::Top, 0),
+            set_margin: (gtk4_layer_shell::Edge::Bottom, 0),
+            set_margin: (gtk4_layer_shell::Edge::Left, 16),
+            set_margin: (gtk4_layer_shell::Edge::Right, 16),
+
+            set_default_height: 32,
+
+            set_title: None,
+            set_decorated: false,
+            set_visible: true,
+
+            gtk::CenterBox {
+                #[wrap(Some)]
+                set_start_widget = &gtk::Box {},
+
+                #[wrap(Some)]
+                set_center_widget = &gtk::Box {},
+
+                #[wrap(Some)]
+                set_end_widget = &gtk::Box {}
+            }
+        }
+    }
+
+    fn init(
+        _init: Self::Init,
+        _root: Self::Root,
+        _sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+        let model = Self {};
+
+        let widgets = view_output!();
+        ComponentParts { model, widgets }
+    }
+}
